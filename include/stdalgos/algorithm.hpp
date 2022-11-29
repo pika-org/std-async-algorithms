d5b2d95aa171dfdf8742b426bd9a659de0ad3bc5
Mikael Simberg <mikael.simberg@iki.fi>
2022-11-29 16:43:31 +0100

    Beginning of P2300 parallel algorithm overloads playground

---
+++ include/stdalgos/algorithm.hpp
@@ -0,0 +1,122 @@
+// TODO: License and copyright
+//
+// Requirements:
+// - must be able to use scheduler from environment (whether
+//   get_completion_scheduler or through some connect-mechanism)
+// - must be able to customize at the top-level (i.e. full algorithm) based on
+//   scheduler
+
+// TODO: Should the callable be sent by the sender or provided immediately?
+// Precedent: then takes f immediately, the value by sender.
+//
+// TODO: Where and how do execution_policy constraints get applied?
+//
+// TODO: Are other hints/properties required besides seq/par/par_unseq?
+//
+// TODO: Some schedulers can provide only maximum seq, no par. Other schedulers
+// can provide only par/par_unseq, no seq (is this true? can use various
+// fallbacks...). How would one recreate (i.e. get exactly the same behaviour)
+// std::for_each(std::execution::par, b, e, f)
+// using the asynchronous overloads?
+// transfer_just(system_context().get_scheduler(), b, e) | stdalgos::for_each(f)
+//
+// TODO: Should seq/par/par_unseq have a relation to forward progress guarantees
+// of a scheduler?
+
+#pragma once
+
+#include <stdexec/execution.hpp>
+#include <stdexec/functional.hpp>
+
+namespace stdalgos {
+namespace detail {
+struct for_each_t {
+  // Overloads have the following priority:
+  // 1. synchronous tag_invoke customization
+  // 2. synchronus default implementation which sync_waits asynchronous
+  //    overload
+  // 3. asynchronous tag_invoke customization with completion scheduler
+  // 4. asynchronous tag_invoke overload on sender
+  // 4. asynchronous default implementation based on bulk
+
+  // Synchronous overload
+  template <stdexec::scheduler Scheduler, typename It, typename F>
+  requires
+      // clang-format off
+      (stdexec::tag_invocable<for_each_t, Scheduler, It, It, F>)
+      // clang-format on
+      stdexec::sender auto
+      operator()(Scheduler &&sched, It b, It e, F &&f) const {
+    // This allows for specializing/optimizing the synchronous case directly
+    stdexec::tag_invoke(*this, std::forward<Scheduler>(sched), b, e,
+                        std::forward<F>(f));
+  }
+
+  template <stdexec::scheduler Scheduler, typename It, typename F>
+  requires
+      // clang-format off
+      (!stdexec::tag_invocable<for_each_t, Scheduler, It, It, F>)
+      // clang-format on
+      stdexec::sender auto
+      operator()(Scheduler &&sched, It b, It e, F &&f) const {
+    // Fall back to synchronizing the asynchronous overload if no synchronous
+    // customization is available.
+    stdexec::this_thread::sync_wait(for_each_t{}(stdexec::transfer_just(
+        std::forward<Scheduler>(sched), b, e, std::forward<F>(f))));
+  }
+
+  // Asynchronous overloads
+
+  // Scheduler customization
+  template <stdexec::sender Sender>
+  requires
+      // clang-format off
+      (stdexec::__tag_invocable_with_completion_scheduler<for_each_t, stdexec::set_value_t, Sender>)
+      // clang-format on
+      stdexec::sender auto
+      operator()(Sender &&sender) const {
+    return stdexec::tag_invoke(
+        stdexec::get_completion_scheduler<stdexec::set_value_t>(sender),
+        std::forward<Sender>(sender));
+  }
+
+  // Sender customization
+  template <stdexec::sender Sender>
+  requires
+      // clang-format off
+      (!stdexec::__tag_invocable_with_completion_scheduler<for_each_t, stdexec::set_value_t, Sender>) &&
+      (stdexec::tag_invocable<for_each_t, Sender>)
+      // clang-format on
+      stdexec::sender auto
+      operator()(Sender &&sender) const {
+    return stdexec::tag_invoke(std::forward<Sender>(sender));
+  }
+  // Default implementation
+  template <stdexec::sender Sender>
+  requires
+      // clang-format off
+      (!stdexec::__tag_invocable_with_completion_scheduler<for_each_t, stdexec::set_value_t, Sender>) &&
+      (!stdexec::tag_invocable<for_each_t, Sender>)
+      // clang-format on
+      stdexec::sender auto
+      operator()(Sender &&sender) const {
+    // This case requires random access iterators. Could fall back to plain for
+    // loop if something weaker. Must still run on scheduler.
+    return stdexec::let_value(
+        std::forward<Sender>(sender), [](auto &b, auto &e, auto &&f) {
+          auto n = std::distance(b, e);
+          return stdexec::just() |
+                 stdexec::bulk(n, [b = std::move(b), e = std::move(e),
+                                   f = std::forward<decltype(f)>(f)](auto i) {
+                   f(b[i]);
+                 });
+        });
+  }
+
+  // TODO: Ranges overloads?
+};
+} // namespace detail
+
+using for_each_t = detail::for_each_t;
+inline constexpr for_each_t for_each{};
+} // namespace stdalgos
---
+++ tests/test1.cpp
@@ -0,0 +1,15 @@
+// TODO: License and copyright
+
+#include <exec/static_thread_pool.hpp>
+#include <iostream>
+#include <std_async_algos/algorithm.hpp>
+#include <stdexec/execution.hpp>
+
+int main() {
+  exec::static_thread_pool pool{2};
+  stdexec::scheduler auto sched = pool.get_scheduler();
+
+  std::vector<int> v{1, 2, 3};
+  stdexec::this_thread::sync_wait(stdalgos::for_each(stdexec::just(
+      v.begin(), v.end(), [](int x) { std::cerr << "x = " << x << '\n'; })));
+}
