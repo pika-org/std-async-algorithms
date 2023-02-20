a9ffa5e70a53c5515b518f843cae9ac8aef101fb
aurianer <aurianer@cscs.ch>
2023-02-20 23:09:53 +0100

    Format tests/for_each.cpp

--- tests/for_each.cpp
+++ tests/for_each.cpp
@@ -18,88 +18,102 @@
 #include <pika/init.hpp>
 #endif
 
-int main(int argc, char *argv[]) {
-  std::vector<int> v{1, 2, 3};
-
-  {
-    exec::static_thread_pool pool{2};
-    stdexec::scheduler auto sched = pool.get_scheduler();
-
-    // TODO: It should be possible to pass a plain execution_policy without
-    // explicitly wrapping it in execution_properties.
-
-    stdalgos::for_each(v.begin(), v.end(), [](int x) { std::cerr << "x = " << x << '\n'; });
-
-    stdalgos::for_each(stdalgos::make_execution_properties(std::execution::par), v.begin(), v.end(),
-                       [](int x) { std::cerr << "x = " << x << '\n'; });
-
-    stdalgos::for_each(sched, v.begin(), v.end(), [](int x) { std::cerr << "x = " << x << '\n'; });
-
-    // NOTE: seq isn't actually taken into account at the moment
-    stdalgos::for_each(stdalgos::with_execution_property(sched, std::execution::seq), v.begin(),
-                       v.end(), [](int x) { std::cerr << "x = " << x << '\n'; });
+int main(int argc, char* argv[])
+{
+    std::vector<int> v{1, 2, 3};
 
     {
-      auto s = stdexec::just(v.begin(), v.end()) |
-               stdalgos::for_each([](int x) { std::cerr << "x = " << x << '\n'; });
-      stdexec::this_thread::sync_wait(std::move(s));
-    }
+        exec::static_thread_pool pool{2};
+        stdexec::scheduler auto sched = pool.get_scheduler();
 
-    {
-      auto s = stdexec::just(v.begin(), v.end()) |
-               stdalgos::for_each(stdalgos::make_execution_properties(std::execution::par),
-                                  [](int x) { std::cerr << "x = " << x << '\n'; });
-      stdexec::this_thread::sync_wait(std::move(s));
-    }
+        // TODO: It should be possible to pass a plain execution_policy without
+        // explicitly wrapping it in execution_properties.
 
-    {
-      auto s = stdexec::just(v.begin(), v.end()) |
-               exec::on(sched, stdalgos::for_each([](int x) { std::cerr << "x = " << x << '\n'; }));
-      stdexec::this_thread::sync_wait(std::move(s));
-    }
+        stdalgos::for_each(v.begin(), v.end(),
+            [](int x) { std::cerr << "x = " << x << '\n'; });
 
-    {
-      auto s = stdexec::just(v.begin(), v.end()) |
-               exec::on(sched,
-                        stdalgos::for_each(stdalgos::make_execution_properties(std::execution::par),
-                                           [](int x) { std::cerr << "x = " << x << '\n'; }));
-      stdexec::this_thread::sync_wait(std::move(s));
+        stdalgos::for_each(
+            stdalgos::make_execution_properties(std::execution::par), v.begin(),
+            v.end(), [](int x) { std::cerr << "x = " << x << '\n'; });
+
+        stdalgos::for_each(sched, v.begin(), v.end(),
+            [](int x) { std::cerr << "x = " << x << '\n'; });
+
+        // NOTE: seq isn't actually taken into account at the moment
+        stdalgos::for_each(
+            stdalgos::with_execution_property(sched, std::execution::seq),
+            v.begin(), v.end(),
+            [](int x) { std::cerr << "x = " << x << '\n'; });
+
+        {
+            auto s = stdexec::just(v.begin(), v.end()) |
+                stdalgos::for_each(
+                    [](int x) { std::cerr << "x = " << x << '\n'; });
+            stdexec::this_thread::sync_wait(std::move(s));
+        }
+
+        {
+            auto s = stdexec::just(v.begin(), v.end()) |
+                stdalgos::for_each(
+                    stdalgos::make_execution_properties(std::execution::par),
+                    [](int x) { std::cerr << "x = " << x << '\n'; });
+            stdexec::this_thread::sync_wait(std::move(s));
+        }
+
+        {
+            auto s = stdexec::just(v.begin(), v.end()) |
+                exec::on(sched, stdalgos::for_each([](int x) {
+                    std::cerr << "x = " << x << '\n';
+                }));
+            stdexec::this_thread::sync_wait(std::move(s));
+        }
+
+        {
+            auto s = stdexec::just(v.begin(), v.end()) |
+                exec::on(sched,
+                    stdalgos::for_each(stdalgos::make_execution_properties(
+                                           std::execution::par),
+                        [](int x) { std::cerr << "x = " << x << '\n'; }));
+            stdexec::this_thread::sync_wait(std::move(s));
+        }
     }
-  }
 
 #ifdef STD_ASYNC_ALGOS_HAVE_PIKA
-  {
-    pika::start(nullptr, argc, argv);
-
     {
-      pika::execution::experimental::thread_pool_scheduler sched{};
+        pika::start(nullptr, argc, argv);
 
-      stdalgos::for_each(sched, v.begin(), v.end(),
-                         [](int x) { std::cerr << "x = " << x << '\n'; });
+        {
+            pika::execution::experimental::thread_pool_scheduler sched{};
 
-      // NOTE: seq isn't actually taken into account at the moment
-      stdalgos::for_each(stdalgos::with_execution_property(sched, std::execution::seq), v.begin(),
-                         v.end(), [](int x) { std::cerr << "x = " << x << '\n'; });
+            stdalgos::for_each(sched, v.begin(), v.end(),
+                [](int x) { std::cerr << "x = " << x << '\n'; });
 
-      {
-        auto s =
-            stdexec::just(v.begin(), v.end()) |
-            exec::on(sched, stdalgos::for_each([](int x) { std::cerr << "x = " << x << '\n'; }));
-        stdexec::this_thread::sync_wait(std::move(s));
-      }
+            // NOTE: seq isn't actually taken into account at the moment
+            stdalgos::for_each(
+                stdalgos::with_execution_property(sched, std::execution::seq),
+                v.begin(), v.end(),
+                [](int x) { std::cerr << "x = " << x << '\n'; });
 
-      {
-        auto s =
-            stdexec::just(v.begin(), v.end()) |
-            exec::on(sched,
-                     stdalgos::for_each(stdalgos::make_execution_properties(std::execution::par),
-                                        [](int x) { std::cerr << "x = " << x << '\n'; }));
-        stdexec::this_thread::sync_wait(std::move(s));
-      }
+            {
+                auto s = stdexec::just(v.begin(), v.end()) |
+                    exec::on(sched, stdalgos::for_each([](int x) {
+                        std::cerr << "x = " << x << '\n';
+                    }));
+                stdexec::this_thread::sync_wait(std::move(s));
+            }
+
+            {
+                auto s = stdexec::just(v.begin(), v.end()) |
+                    exec::on(sched,
+                        stdalgos::for_each(stdalgos::make_execution_properties(
+                                               std::execution::par),
+                            [](int x) { std::cerr << "x = " << x << '\n'; }));
+                stdexec::this_thread::sync_wait(std::move(s));
+            }
+        }
+
+        pika::finalize();
+        pika::stop();
     }
-
-    pika::finalize();
-    pika::stop();
-  }
 #endif
 }
