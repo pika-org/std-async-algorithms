11b44ebda1dc9375ede07a9e41aa99ee90f2a6a9
Mikael Simberg <mikael.simberg@iki.fi>
2023-04-05 11:29:05 +0200

    Remove execution properties (#10)

--- include/stdalgos/detail/merge.hpp
+++ include/stdalgos/detail/merge.hpp
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include <stdalgos/execution_properties.hpp>
 #include <stdexec/execution.hpp>
 #include <stdexec/functional.hpp>
 
@@ -42,30 +41,20 @@ namespace stdalgos {
         {
             // Synchronous overloads
 
+            // This should be identical to the existing synchronous merge
+            // overload in std. Included only for completeness. This is not
+            // meant to be customized. It should fall back to the very default
+            // implementation.
             template <typename InputIt1, typename InputIt2, typename OutputIt>
             OutputIt
             operator()(InputIt1 b1, InputIt1 e1, InputIt2 b2, InputIt2 e2, OutputIt d) const
             {
                 // Fall back to synchronizing the asynchronous overload if no synchronous
                 // customization is available.
-                return merge_t{}(execution_properties{}, b1, e1, b2, e2, d);
-            }
-
-            // This should be identical to the existing merge(exec_policy, ...)
-            // overload in std. Included only for completeness. This is not meant to be
-            // customized. It should fall back to the very default implementation.
-            template <typename InputIt1, typename InputIt2, typename OutputIt,
-                typename... Properties>
-            OutputIt operator()(execution_properties<Properties...> const& exec_properties,
-                InputIt1 b1, InputIt1 e1, InputIt2 b2, InputIt2 e2, OutputIt d) const
-            {
-                // Fall back to synchronizing the asynchronous overload if no synchronous
-                // customization is available.
                 // The sync_wait sender needs unwrapping since it returns an optional of tuple. And this
                 // overload returns an the output iterator.
-                return std::get<0>(stdexec::this_thread::sync_wait(
-                    merge_t{}(stdexec::just(b1, e1, b2, e2, d), exec_properties))
-                                       .value());
+                return std::get<0>(
+                    stdexec::sync_wait(merge_t{}(stdexec::just(b1, e1, b2, e2, d))).value());
             }
 
             // TODO: add the overload that requires tag_invocable merge
@@ -77,7 +66,7 @@ namespace stdalgos {
                 typename OutputIt>
             requires
                 // clang-format off
-      (!stdexec::tag_invocable<merge_t, Scheduler, InputIt1, InputIt1, InputIt2, InputIt2, OutputIt>)
+                (!stdexec::tag_invocable<merge_t, Scheduler, InputIt1, InputIt1, InputIt2, InputIt2, OutputIt>)
                 // clang-format on
                 OutputIt
                 operator()(Scheduler&& sched, InputIt1 b1, InputIt2 e1, InputIt2 b2, InputIt1 e2,
@@ -85,72 +74,30 @@ namespace stdalgos {
             {
                 // The sync_wait sender needs unwrapping since it returns an optional of tuple. And this
                 // overload returns an the output iterator.
-                return std::get<0>(stdexec::this_thread::sync_wait(
-                    merge_t{}(
-                        stdexec::transfer_just(std::forward<Scheduler>(sched), b1, e1, b2, e2, d)))
-                                       .value());
+                return std::get<0>(
+                    stdexec::sync_wait(merge_t{}(stdexec::transfer_just(
+                                           std::forward<Scheduler>(sched), b1, e1, b2, e2, d)))
+                        .value());
             }
 
             // Asynchronous overloads
 
-            // Use empty execution properties by default, forward to other overloads
-            template <stdexec::sender Sender>
-            stdexec::sender auto operator()(Sender&& sender) const
-            {
-                return merge_t{}(std::forward<Sender>(sender), execution_properties<>{});
-            }
-
             // TODO: Add the overloads with tag_invocable and not tag_invocable with completion scheduler
 
             // Default implementation
-            template <stdexec::sender Sender, typename... Properties>
+            template <stdexec::sender Sender>
             requires
                 // clang-format off
-      (!stdexec::__tag_invocable_with_completion_scheduler<merge_t, stdexec::set_value_t, Sender>) &&
-      (!stdexec::tag_invocable<merge_t, Sender, execution_properties<Properties...> const&>)
+                (!stdexec::__tag_invocable_with_completion_scheduler<merge_t, stdexec::set_value_t, Sender>) &&
+                (!stdexec::tag_invocable<merge_t, Sender>)
                 // clang-format on
                 stdexec::sender auto
-                operator()(Sender&& sender,
-                    execution_properties<Properties...> const& exec_properties) const
+                operator()(Sender&& sender) const
             {
                 // TODO: Should a (completion) scheduler be required?
-                if constexpr (stdexec::__has_completion_scheduler<Sender, stdexec::set_value_t>)
-                {
-                    stdexec::scheduler auto sched =
-                        stdexec::get_completion_scheduler<stdexec::set_value_t>(
-                            stdexec::get_env(sender));
-                    // TODO: remove the let_value, once the problem of chaining two transfers is
-                    // solved in stdexec
-                    return stdexec::let_value(std::forward<Sender>(sender),
-                        [sched = std::move(sched), exec_properties](
-                            auto b1, auto e1, auto b2, auto e2, auto d) {
-                            // TODO: This could use something a bit nicer.
-                            stdexec::scheduler auto sched_with_properties =
-                                with_execution_properties(sched, exec_properties);
-
-                            return stdexec::schedule(std::move(sched)) |
-                                stdexec::then(
-                                    [=]() mutable { return merge_impl(b1, e1, b2, e2, d); });
-                        });
-                }
-                else
-                {
-                    // If there's no completion scheduler there's nothing to apply the
-                    // execution policy or properties to.
-                    // TODO: Should this fall back to the system_context scheduler? Possibly
-                    // yes, especially if the execution policy is par.
-                    return std::forward<Sender>(sender) |
-                        stdexec::then([](auto b1, auto e1, auto b2, auto e2, auto d) mutable {
-                            return merge_impl(b1, e1, b2, e2, d);
-                        });
-                }
-            }
-
-            template <typename... Properties>
-            stdexec::__binder_back<merge_t, execution_properties<Properties...>>
-            operator()(execution_properties<Properties...> const& exec_properties) const
-            {
-                return {{}, {}, {exec_properties}};
+                return std::forward<Sender>(sender) | stdexec::then([](auto&&... ts) {
+                    return merge_impl(std::forward<decltype(ts)>(ts)...);
+                });
             }
 
             stdexec::__binder_back<merge_t> operator()() const
